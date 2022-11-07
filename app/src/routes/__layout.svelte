d8c0f047e33a44d88ad1813bcd2b9cd1187bf0ed
theArctesian <stephen.d.okita@gmail.com>
2022-11-07 19:29:02 +0800

    a

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -27,7 +27,7 @@
 		School |
 	</a>
 </div>
-<Countdown from="2022-12-16" dateFormat="YYYY-MM-DD" zone="Asia/Hong_Kong" let:remaining>
+<Countdown from="2022-12-16 00:00:00" dateFormat="YYYY-MM-DD H:m:s" zone="Asia/Hong_Kong" let:remaining>
 	<div class="count text-nosferatu-100 text-center bg-nosfeatu-300 transition-all duration-100 ">
 		{#if remaining.done === false}
 			<span style="color: #ff79c6;">Time till Christmas</span>
