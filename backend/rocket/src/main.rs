e0f40b601fdc227f3fc135f9d08c312cbf7ee44d
theArctesian <stephen.d.okita@gmail.com>
2022-12-15 09:11:01 +0800

    hello again

--- backend/rocket/src/main.rs
+++ backend/rocket/src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
 use rocket::{get, http::Status, serde::json::Json};
-use serde_json::{Number, Value};
+// use serde_json::{Number, Value};
 
 #[get("/")]
 fn hello() -> Result<Json<String>, Status> {
@@ -10,11 +10,35 @@ fn hello() -> Result<Json<String>, Status> {
 
 
 #[get("/todo")]
-fn hello() -> Result<Json<String>, Status> {
-    Ok(Json(String::from("Hello from rust and mongoDB")))
+fn todoHeader() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Todo is working")))
 }
 
+#[get("/todo/school")]
+fn todoSchool() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("school todo sample")))
+}
+
+#[get("/todo/watch")]
+fn todoWatch() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("to watch sample")))
+}
+
+#[get("/todo/read")]
+fn todoRead() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("to read sample")))
+}
+
+#[get("/todo/make")]
+fn todoMake() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("to make sample")))
+}
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![hello])
+    .mount("/todo", routes![todoHeader])
+    .mount("/todo/school", routes![todoSchool])
+    .mount("/todo/watch", routes![todoWatch])
+    .mount("/todo/read", routes![todoRead])
+    .mount("/todo/make", routes![todoMake])
 }
