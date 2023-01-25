919dea0f57b0fd27ce414f605015cee32d3d2ece
theArctesian <stephen.d.okita@gmail.com>
2023-01-25 15:23:10 +0800

    work?

--- app/src/compontents/Dev/server.svelte
+++ app/src/compontents/Dev/server.svelte
@@ -1,3 +1,25 @@
+<div class="p-5 flex flex-wrap main flex-col">
+	<h1 class="text-2xl text-center text-nosferatu-100 my-2">Server</h1>
+	<div class=" flex m-1">
+		<img alt="github logo" src="https://img.icons8.com/ios-filled/344/linode.png" />
+		<a href="https://cloud.linode.com/linodes" target="_blank">Linode</a>
+	</div>
+	<div class=" flex m-1">
+		<img alt="github logo" src="https://img.icons8.com/ios/344/google-cloud-platform.png" />
+		<a href="https://domains.google.com/registrar/?hl=en-US&pli=1&authuser=1" target="_blank">
+			GCP
+		</a>
+	</div>
+	<div class=" flex m-1">
+		<img alt="github logo" src="https://img.icons8.com/windows/344/domain-name.png" />
+		<a href="https://registrar.epik.com/domain/portfolio" target="_blank">Epik</a>
+	</div>
+	<div class=" flex m-1">
+		<img alt="github logo" src="https://simpleicons.org/icons/vercel.svg" />
+		<a href="https://vercel.com/dashboard" target="_blank">Vercel</a>
+	</div>
+</div>
+
 <style>
 	img {
 		width: 2.5rem;
@@ -27,21 +49,3 @@
 		}
 	}
 </style>
-
-<div class="p-5 flex flex-wrap main flex-col">
-	<h1 class="text-2xl text-center text-nosferatu-100 my-2">Server</h1>
-	<div class=" flex m-1">
-		<img alt="github logo" src="https://img.icons8.com/ios-filled/344/linode.png" />
-		<a href="https://cloud.linode.com/linodes" target="_blank">Linode</a>
-	</div>
-	<div class=" flex m-1">
-		<img alt="github logo" src="https://img.icons8.com/ios/344/google-cloud-platform.png" />
-		<a href="https://domains.google.com/registrar/?hl=en-US&pli=1&authuser=1" target="_blank">
-			GCP
-		</a>
-	</div>
-	<div class=" flex m-1">
-		<img alt="github logo" src="https://img.icons8.com/windows/344/domain-name.png" />
-		<a href="https://registrar.epik.com/domain/portfolio" target="_blank">Epik</a>
-	</div>
-</div>
